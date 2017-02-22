30480d8f4f89ee368e537562dd4710cf2317b491
Christian Kniep <christian@qnib.org>
2017-02-22 15:04:40 -0800

    configure HOSTNAME

--- opt/qnib/entry/20-kafka.sh
+++ opt/qnib/entry/20-kafka.sh
@@ -3,4 +3,9 @@
 
 mkdir -p /opt/kafka/config
 echo "[II] Write config using: KAFKA_PORT=${KAFKA_PORT}, ZK_SERVERS=${ZK_SERVERS}, KAFKA_BROKER_ID=${KAFKA_BROKER_ID}"
-cat /opt/qnib/kafka/conf/server.properties | sed -e "s/KAFKA_PORT/${KAFKA_PORT}/" -e "s/ZK_SERVERS/${ZK_SERVERS}/" -e "s/KAFKA_BROKER_ID/${KAFKA_BROKER_ID}/" > /opt/kafka/config/server.properties
+cat /opt/qnib/kafka/conf/server.properties \
+   | sed -e "s/KAFKA_PORT/${KAFKA_PORT}/" \
+         -e "s/ZK_SERVERS/${ZK_SERVERS}/" \
+         -e "s/KAFKA_BROKER_ID/${KAFKA_BROKER_ID}/" \
+         -e "s/HOSTNAME/${HOSTNAME}/" \
+   > /opt/kafka/config/server.properties
