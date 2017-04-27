1666f72f2904ab5919660676d8b56dc683469b9d
htonnies <htonnies@gaikai.com>
2017-04-27 12:05:46 +0200

    Add config for message codec's
    * inter.broker.protocol.version
    * log.message.format.version

--- opt/qnib/entry/20-kafka.sh
+++ opt/qnib/entry/20-kafka.sh
@@ -12,10 +12,12 @@ if [[ -z ${ADVERTISED_LISTENERS} ]];then
 fi
 
 mkdir -p /opt/kafka/config
-echo "[II] Write config using: KAFKA_PORT=${KAFKA_PORT}, ZK_SERVERS=${ZK_SERVERS}, KAFKA_BROKER_ID=${KAFKA_BROKER_ID}"
+echo "[II] Write config using: KAFKA_PORT=${KAFKA_PORT}, ZK_SERVERS=${ZK_SERVERS}, KAFKA_BROKER_ID=${KAFKA_BROKER_ID}, INTER_BROKER_PROTOCOL_VERSION=${INTER_BROKER_PROTOCOL_VERSION}, LOG_MESSAGE_FORMAT_VERSION=${LOG_MESSAGE_FORMAT_VERSION}"
 cat /opt/qnib/kafka/conf/server.properties \
    | sed -e "s/KAFKA_PORT/${KAFKA_PORT}/" \
          -e "s/ZK_SERVERS/${ZK_SERVERS}/" \
          -e "s/KAFKA_BROKER_ID/${KAFKA_BROKER_ID}/" \
          -e "s/ADVERTISED_LISTENERS/${ADVERTISED_LISTENERS}/" \
+         -e "s/INTER_BROKER_PROTOCOL_VERSION/${INTER_BROKER_PROTOCOL_VERSION}/" \
+         -e "s/LOG_MESSAGE_FORMAT_VERSION/${LOG_MESSAGE_FORMAT_VERSION}/" \
    > /opt/kafka/config/server.properties
